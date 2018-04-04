1374912670c31e8fc73590d1e5c4b8d640e43cf8
Aditya Kumar Sharma <adityas@EVOLVUS.COM>
2018-04-04 19:20:10 +0530

    MargedCode3

--- api/controllers/processReq.js
+++ api/controllers/processReq.js
@@ -3,57 +3,41 @@
 var http  = require('http'),
     https = require('https'),
     aws4  = require('aws4');
+var rp   = require('request-promise');
 
- var rp   = require('request-promise');
-
-//var Regex = require("regex");
-
-//var opts = require('./lexconnect');
 var crypto = require('crypto'),
 key = 'jenson';
+
 var mongoose = require('mongoose'),
-  //Task = mongoose.model('Tasks');
 blacklistcheck = mongoose.model('blacklist');
 
 var mongoose = require('mongoose'),
 ciservice = mongoose.model('ciservice');
-//Task1 = mongoose.model();
-//
+
 var request = require('request');
-
-
 
 exports.handlerequest = function(req, res) {
   console.log(req.body);
   var val = req.body.input;
       console.log('in');
+    var    bodytext = '{"inputText" : "'+val+'"}';
+    console.log(bodytext);
 
-
-
-
-var opts = {
+  var opts = {
          host: 'runtime.lex.us-east-1.amazonaws.com',
-// 		    //host:'10.10.69.203'
          service: 'lex',
          region: 'us-east-1',
          uri: 'https://runtime.lex.us-east-1.amazonaws.com/bot/dianaBot/alias/dianaServer/user/shrimank/text',
          path: 'bot/dianaBot/alias/dianaServer/user/shrimank/text',
-          body: '{ "inputText": "hi"  }'
+         body : bodytext
          };
-         //
+         console.log(opts.body);
+
          blacklistcheck.find({}, function(err, task) {
            if (err){
              res.send(err);
            }else{
-            // b = {"inputText" : val};
-              // b = { "inputText": "hi"  };
 
-              // console.log(b);
-              // console.log(typeof(b));
-              // console.log(typeof(b.tostring()));
-              // console.log(b.tostring());
-
-              //opts.body =  b.tostring();
              for (var i=0 ; i < task.length ; i++){
                  var checkval = new RegExp(task[i].pattern.toString());
                  if (checkval.test(val)) {
@@ -88,13 +72,7 @@ var opts = {
                )
              .catch( (e)=> console.log('failed:'+e));
            };
-
          });
-
-
            }
          });
-
-
-
 };
