35755c0241aa35ed29ddddc847c53132fe209d53
Aditya Kumar Sharma <adityas@EVOLVUS.COM>
2018-04-17 16:10:42 +0530

    dfreq

--- api/controllers/dfprocessReq.js
+++ api/controllers/dfprocessReq.js
@@ -225,7 +225,7 @@ var headers = {
 var options = {
 url: `https://api.dialogflow.com/v1/query?v=20150910&lang=en&query=${inputext}&sessionId=12345`,
 headers: headers
-qs: {'key1': 'xxx', 'key2': 'yyy'}
+//qs: {'key1': 'xxx', 'key2': 'yyy'}
 }
 
 // Start the request
