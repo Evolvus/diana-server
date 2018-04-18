f799b5da2fd26e569afe3d5bc252ec1369058680
Aditya Kumar Sharma <adityas@EVOLVUS.COM>
2018-04-18 14:25:19 +0530

    dfreq

--- api/controllers/dfprocessintents.js
+++ api/controllers/dfprocessintents.js
@@ -42,8 +42,10 @@ request.body.input.requestAttributes.auditid=request.body.input.bodyjson.result.
 console.log("contecxt inside df intent",request.body.input.bodyjson.result.contexts);
 console.log("contecxt inside df intentname",request.body.input.bodyjson.result.contexts[0].name);
 console.log(typeof(request.body.input.bodyjson.result.contexts[0].name));
-console.log(JSON.parse("[" + request.body.input.bodyjson.result.contexts[0].name + "]"));
-console.log(typeof(JSON.parse("[" + request.body.input.bodyjson.result.contexts[0].name + "]")));
+var stringname = request.body.input.bodyjson.result.contexts[0].name;
+var arrayname = stringname.split(', ');
+console.log(arrayname);
+console.log(typeof(arrayname));
 console.log("contecxt inside df intentname0",request.body.input.bodyjson.result.contexts[0].name[0]);
 console.log("contecxt inside df intentname1",request.body.input.bodyjson.result.contexts[0].name[1]);
 //event.session.attributes = {auditid : '123', channelid : 'Alexa'};
